f9f7f5bbf0d055718245cfa5cfeeb2cb1a84aebe
L❤️ ☮️ ✋ <6723574+louisgv@users.noreply.github.com>
2025-02-26 14:57:31 -0800

    chore: add import order

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -7,4 +7,18 @@ export default {
   jsxSingleQuote: true,
   proseWrap: 'never',
   plugins: ['@ianvs/prettier-plugin-sort-imports'],
+  importOrder: [
+    '<TYPES>',
+    '<TYPES>^[@/]',
+    '<TYPES>^[./|../]',
+    '',
+
+    '<BUILTIN_MODULES>', // Node.js built-in modules
+    '<THIRD_PARTY_MODULES>', // Imports not matched by other special words or groups.
+    '',
+
+    '^[../]',
+    '^[./]',
+  ],
+  importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
 };
